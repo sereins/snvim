a7bba5454cf2c5a37679ed0961e91d54deb01ee9
sereins <137034198@qq.com>
2022-11-09 23:36:24 +0800

    基本配置

---
+++ init.lua
@@ -0,0 +1,16 @@
+require('plugins')
+
+--显示行号
+vim.o.number = true
+--tab变为空格且
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+--有关主题换颜色的配置
+vim.o.termguicolors = true
+--自动换行
+vim.smartindent = true
+--所在行高亮
+vim.o.cursorline = true
+--leader 的配置
+vim.g.mapleader = ','
